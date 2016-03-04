4ebb8f433c1e934697645b6b78e84a30bd9d3ed5
Pete <pmckinney8806@gmail.com>
2016-03-04 11:24:15 -0600

    created repository and xkcdbot.js. Edited the .gitignore.

---
+++ xkcdbot.js
@@ -0,0 +1,65 @@
+'use strict'
+
+var SlackBot = require('slackbots');
+var fs, configurationFile;
+
+configurationFile = 'botinfo.json';
+fs = require('fs');
+
+var config = JSON.parse(fs.readFileSync(configurationFile));
+
+var bot = new SlackBot({
+  name: config.name,
+  token: config.token
+});
+
+bot.on('start', function(){
+  bot.postMessage(config.devchannel,
+    'I am alive!');
+});
+
+
+bot.on('message', function(message) {
+  if(message.text == 'xkcd'){
+    var url = 'http://xkcd.com/';
+    var infoUrl = url + 'info.0.json';
+
+    require('request')(infoUrl, function(error,response,body){
+      try{
+        var xkcdInfo = JSON.parse(body);
+        var id = Math.floor((Math.random() * xkcdInfo.num) + 1);
+
+        while(id == 404){
+          id = Math.floor((Math.random() * xkcdInfo.num) + 1);
+        }
+
+        url += id + '/info.0.json';
+        requestComic(bot,message,url);
+      }
+      catch(e){
+        console.log(e);
+      }
+    });
+  }
+});
+
+
+function requestComic(xkcdbot, message, url){
+  require('request')(url,function(error,result,body){
+    try{
+      console.log(body);
+      var comic = JSON.parse(body);
+
+      var params = {
+        icon_emoji: ':xkcd:'
+      };
+
+      xkcdbot.postMessage(message.channel,
+        comic.title + '\n' + comic.img,
+        params);
+    }
+    catch(e){
+      console.log('unable to send message');
+    }
+  })
+}
